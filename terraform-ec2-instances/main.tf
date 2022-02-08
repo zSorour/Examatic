4e7e55c60dd2835b00ee4c14e456bf868df4844d
zSorour <ahmad_sorour@outlook.com>
2022-02-08 11:04:18 +0200

    separate files for data-providers and variables

--- terraform-ec2-instances/main.tf
+++ terraform-ec2-instances/main.tf
@@ -17,27 +17,6 @@ provider "aws" {
 resource "aws_default_vpc" "default_vpc" {}
 
 
-# data provider to get information about subnet ids of a certain VPC
-data "aws_subnet_ids" "default_subnets" {
-  vpc_id = aws_default_vpc.default_vpc.id
-}
-
-
-# data provider to get the latest ubuntu ami from AWS servers
-data "aws_ami" "latest_ubuntu_ami" {
-  most_recent = true
-  owners      = ["099720109477"] #AMI created by Canonical
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-
 # create security group
 resource "aws_security_group" "http_server_sg" {
   name   = "http_server_sg"
