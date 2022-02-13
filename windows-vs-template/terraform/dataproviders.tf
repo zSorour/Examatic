f64f44e65427d9332516d8c5a180cc6f17dcb23d
zSorour <ahmad_sorour@outlook.com>
2022-02-13 20:46:54 +0200

    Added demo project for provisioning Windows instances with Visual Studio

---
+++ windows-vs-template/terraform/dataproviders.tf
@@ -0,0 +1,22 @@
+# data provider to get information about subnet ids of a certain VPC
+data "aws_subnets" "default_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default_vpc.id]
+  }
+}
+
+
+# data provider to get the latest ubuntu ami from AWS servers
+data "aws_ami" "latest_windows_ami" {
+  most_recent = true
+  owners      = ["801119661308"] #AMI created by Microsoft
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
