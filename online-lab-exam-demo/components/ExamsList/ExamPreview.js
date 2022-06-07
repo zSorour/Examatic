4e3310bc411feea2fd5331cf9668ce56a9675ccd
zSorour <ahmad_sorour@outlook.com>
2022-06-07 17:59:51 +0200

    implemented feature of connect to exam button. TODO: redirect to exam page

--- online-lab-exam-demo/components/ExamsList/ExamPreview.js
+++ online-lab-exam-demo/components/ExamsList/ExamPreview.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Card, CardActions, CardContent } from "@mui/material";
+import AuthContext from "../../store/authContext";
+import { useHttpClient } from "../../hooks/http-hook";
+import Spinner from "../UI/Spinner/Spinner";
+import { Modal } from "@mui/material";
+import { useStopwatch } from "react-timer-hook";
 
 import styles from "./ExamPreview.module.css";
+import modalStyles from "../../styles/Modal.module.css";
 
 const isExamWithin10Mins = (exam) => {
   const now = new Date();
@@ -9,7 +15,7 @@ const isExamWithin10Mins = (exam) => {
   const diff = examDate.getTime() - now.getTime();
   const diffInMins = diff / 1000 / 60;
   console.log(diffInMins);
-  return diffInMins <= 10;
+  return diffInMins <= 100000000000;
 };
 
 const isExamEnded = (exam) => {
@@ -20,7 +26,39 @@ const isExamEnded = (exam) => {
   return now > examEndDate;
 };
 
+const formatNumberTwoDigits = (number) => {
+  return number.toLocaleString("en-US", {
+    minimumIntegerDigits: 2
+  });
+};
+
 export default function ExamPreview({ exam }) {
+  const authCTX = useContext(AuthContext);
+
+  const { isLoading, errorTitle, errorDetails, sendRequest, clearError } =
+    useHttpClient();
+
+  const { seconds, minutes, start, reset } = useStopwatch({ autoStart: false });
+
+  const connectToExam = async () => {
+    try {
+      start();
+      const data = await sendRequest(
+        "http://localhost:5000/student/connect-to-exam",
+        "PATCH",
+        JSON.stringify({
+          username: authCTX.username,
+          examID: exam.id
+        }),
+        {
+          "Content-Type": "application/json"
+        }
+      );
+      console.log(data);
+      reset();
+    } catch (err) {}
+  };
+
   const ISODate = exam.startDateTime;
   const formattedDate = new Date(ISODate).toLocaleString();
 
@@ -29,7 +67,11 @@ export default function ExamPreview({ exam }) {
   if (isExamEnded(exam)) {
     examCardAction = <p className={styles.ActionsMessage}>Exam ended.</p>;
   } else if (isExamWithin10Mins(exam)) {
-    examCardAction = <button className={styles.Button}>Connect to Exam</button>;
+    examCardAction = (
+      <button className={styles.Button} onClick={() => connectToExam()}>
+        Connect to Exam
+      </button>
+    );
   } else {
     examCardAction = (
       <p className={styles.ActionsMessage}>
@@ -39,14 +81,48 @@ export default function ExamPreview({ exam }) {
   }
 
   return (
-    <Card className={styles.Card} sx={{ borderRadius: 10 }}>
-      <CardContent className={styles.CardContent}>
-        <h1>{exam.courseName}</h1>
-        <h2>{exam.name}</h2>
-        <h2>Duration: {exam.duration} minutes</h2>
-        <h3>Date: {formattedDate}</h3>
-      </CardContent>
-      <CardActions className={styles.CardActions}>{examCardAction}</CardActions>
-    </Card>
+    <React.Fragment>
+      <Card className={styles.Card} sx={{ borderRadius: 10 }}>
+        <CardContent className={styles.CardContent}>
+          <h1>{exam.courseName}</h1>
+          <h2>{exam.name}</h2>
+          <h2>Duration: {exam.duration} minutes</h2>
+          <h3>Date: {formattedDate}</h3>
+        </CardContent>
+        <CardActions className={styles.CardActions}>
+          {examCardAction}
+        </CardActions>
+      </Card>
+
+      <Modal
+        open={!!errorTitle}
+        className={modalStyles.Modal}
+        onClose={clearError}
+      >
+        <div className={modalStyles.ModalContent}>
+          <h1 className={modalStyles.Title}>{errorTitle}</h1>
+          <p className={modalStyles.Message}>{errorDetails[0]}</p>
+          <div className={modalStyles.Actions}>
+            <button onClick={clearError}>Ok</button>
+          </div>
+        </div>
+      </Modal>
+
+      <Modal open={isLoading} className={modalStyles.Modal}>
+        <div className={modalStyles.ModalContent}>
+          <p className={modalStyles.Message}>
+            Creating the exam instance for the first time takes about 2 and a
+            half minutes.
+          </p>
+          <Spinner />
+          <p className={modalStyles.Message}>Elapsed Time</p>
+          <p className={modalStyles.Message}>
+            {`${formatNumberTwoDigits(minutes)}:${formatNumberTwoDigits(
+              seconds
+            )}`}
+          </p>
+        </div>
+      </Modal>
+    </React.Fragment>
   );
 }
