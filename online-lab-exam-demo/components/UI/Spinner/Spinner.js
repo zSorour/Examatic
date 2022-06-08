ffe2e80bd071647da0b20803db1abb64fa17202e
zSorour <ahmad_sorour@outlook.com>
2022-06-08 20:51:45 +0200

    changed spinner loader style

--- online-lab-exam-demo/components/UI/Spinner/Spinner.js
+++ online-lab-exam-demo/components/UI/Spinner/Spinner.js
@@ -5,7 +5,20 @@ import styles from "./Spinner.module.css";
 const Spinner = () => {
   return (
     <div>
-      <div className={styles.DualRing}></div>
+      <div className={styles["sk-folding-cube"]}>
+        <div
+          className={[styles["sk-cube1"], styles["sk-cube"]].join(" ")}
+        ></div>
+        <div
+          className={[styles["sk-cube2"], styles["sk-cube"]].join(" ")}
+        ></div>
+        <div
+          className={[styles["sk-cube3"], styles["sk-cube"]].join(" ")}
+        ></div>
+        <div
+          className={[styles["sk-cube4"], styles["sk-cube"]].join(" ")}
+        ></div>
+      </div>
     </div>
   );
 };
