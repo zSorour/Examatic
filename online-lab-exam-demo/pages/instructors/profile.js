33d330ce64504e9dc0e41b82526d4b5a06a8ca7b
zSorour <ahmad_sorour@outlook.com>
2022-06-08 13:15:06 +0200

    Implemented sign out functionality for both student and instructor.

---
+++ online-lab-exam-demo/pages/instructors/profile.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import AuthContext from "../../store/auth-context/authContext";
+import { useRouter } from "next/router";
+
+export default function InstructorProfilePage() {
+  const authCTX = useContext(AuthContext);
+  const router = useRouter();
+
+  const logout = () => {
+    authCTX.logout();
+    router.push("/login");
+  };
+
+  return (
+    <div>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+}
--- online-lab-exam-demo/pages/students/profile.js
+++ online-lab-exam-demo/pages/students/profile.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from "../../store/auth-context/authContext";
+import { useRouter } from "next/router";
 
-export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+export default function StudentProfilePage() {
+  const authCTX = useContext(AuthContext);
+  const router = useRouter();
+
+  const logout = () => {
+    authCTX.logout();
+    router.push("/login");
+  };
+
+  return (
+    <div>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
 }
