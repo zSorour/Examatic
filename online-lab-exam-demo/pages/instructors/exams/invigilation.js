d038d96cbb80807f55bb22c8bc6ea416162c9159
zSorour <ahmad_sorour@outlook.com>
2022-06-14 17:23:08 +0200

    added updating invigilation info on receiving socketID from backend

--- online-lab-exam-demo/pages/instructors/exams/invigilation.js
+++ online-lab-exam-demo/pages/instructors/exams/invigilation.js
@@ -1,19 +1,44 @@
 import React, { useState, useRef, useEffect } from "react";
+import { useRouter } from "next/router";
 
 import Peer from "simple-peer";
 import SocketIOClient from "socket.io-client";
 import StudentsMediaVideoList from "../../../components/Invigilation/StudentsMediaVideoList";
+import { useHttpClient } from "../../../hooks/http-hook";
 
 import styles from "./Invigilation.module.css";
 
 export default function InvigilationPage() {
   const socket = useRef(null);
   const [studentStreams, setStudentStreams] = useState(new Map());
-  const [mySocketID, setMySocketID] = useState(null);
-
   // a function to put value into the state map immutably.
   const putInStudentStreamsMap = (k, v) => {
     setStudentStreams(new Map(studentStreams.set(k, v)));
+  };
+
+  const router = useRouter();
+
+  const { isLoading, errorTitle, errorDetails, sendRequest, clearError } =
+    useHttpClient();
+
+  const getIPAddress = async () => {
+    return await sendRequest("https://geolocation-db.com/json/", "GET");
+  };
+
+  const updateInvigilationInstanceInfo = async (socketID) => {
+    const { IPv4 } = await getIPAddress();
+    const examID = router.query.examID;
+    const invigilationInfoUpdate = {
+      socketID: id,
+      instanceIP: IPv4,
+      examID: examID
+    };
+    await sendRequest(
+      "http://localhost:5000/exam-management/update-exam-invigilation-info",
+      "PATCH",
+      JSON.stringify(invigilationInfoUpdate),
+      { "Content-Type": "application/json" }
+    );
   };
 
   useEffect(() => {
@@ -21,13 +46,12 @@ export default function InvigilationPage() {
       path: "/invigilation"
     });
 
-    socket.current.on("givenSocketID", (id) => {
-      setMySocketID(id);
+    socket.current.on("givenSocketID", async (id) => {
+      await updateInvigilationInstanceInfo(id);
     });
 
     socket.current.on("incomingConnection", (data) => {
       const { studentSocketID, username, signal } = data;
-      console.log("incoming connection: ", data);
       acceptIncomingConnection(username, studentSocketID, signal);
     });
   }, []);
@@ -56,11 +80,9 @@ export default function InvigilationPage() {
       On receiving stream from student, add it to the map of streams keyed to the student username.
     */
     peer.on("stream", (stream) => {
-      console.log("Received stream!!!");
       putInStudentStreamsMap(studentUsername, stream);
     });
 
-    // ??? is this needed?
     peer.signal(studentSignal);
   };
 
